93736a5b5e94313f3e79fe07a03f250d92630f2c
leoCardosoDev <leocardosodev@gmail.com>
2023-11-27 08:03:51 -0300

    feat: add event handle interface

--- pkg/events/interfaces.go
+++ pkg/events/interfaces.go
@@ -11,3 +11,11 @@ type EventInterface interface {
 type EventHandlerInterface interface {
 	Handle(event EventInterface)
 }
+
+type EventDispatcherInterface interface {
+	Register(eventName string, handler EventHandlerInterface) error
+	Dispatch(event EventInterface) error
+	Remove(eventName string, handler EventHandlerInterface) error
+	Has(eventName string, handler EventHandlerInterface) bool
+	Clear() error
+}
